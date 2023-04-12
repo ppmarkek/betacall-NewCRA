d465c4de7c6431825ca2ae45e442c932cffd8eab
ppmarkek <markungeitis01@gmail.com>
2023-04-12 22:39:19 +0300

    feat: add Step3

---
+++ src/components/pages/SignUp/Step3/Step3.tsx
@@ -0,0 +1,28 @@
+import { StyledLink, Wrapper } from './style';
+import Undraw from '../../../../assets/Image/Undraw.svg';
+import { Grid } from '@mui/material';
+import Text from '../../../atoms/Text/Text';
+
+const Step3 = () => {
+    return (
+        <Wrapper container>
+            <img src={Undraw} alt='' />
+            <Grid container gap={'4px'} width={'465px'} justifyContent={'center'}>
+                <Text variant={'H1'} small={''} color={'#1A1C1D'}>
+          Thank you
+                </Text>
+                <Grid textAlign={'center'}>
+                    <Text variant={'LIGHT'} small={''} color={''}>
+            We sent an email to catherine.shaw@gmail.com
+                    </Text>
+                    <Text variant={'LIGHT'} small={''} color={''}>
+            Click confirmation link in the email to verify your account
+                    </Text>
+                </Grid>
+            </Grid>
+            <StyledLink to={''}>Resend Email</StyledLink>
+        </Wrapper>
+    );
+};
+
+export default Step3;
---
+++ src/components/pages/SignUp/Step3/style.ts
@@ -0,0 +1,21 @@
+import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Wrapper = styled(Grid)`
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 935px;
+  gap: 44px;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #6b59cc;
+  font-family: 'Lato';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+`;
